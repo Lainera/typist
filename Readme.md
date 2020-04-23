9b1987d29376fbac440c51ba548aff85d882f39d
Lainera <fistlain@hotmail.com>
2020-04-22 23:09:26 -0700

    Init

---
+++ Readme.md
@@ -0,0 +1,15 @@
+## Type away
+
+Goal: be able to practice typing with learning materials, articles, code etc. 
+- Speed, instantaneous, average. 
+- Mistakes, keep count
+- Rolling text, displays how many symbols left. Up to three lines at a time.
+- Colors the output? potentially. Given we are on the last character of the line, line contains no errors and we hit enter -> line disappears.
+---
+
+So need top line for ui/metrics. 
+Need to put a cursor on a string.
+Need to listen to input from keyboard. 
+Character in -> is it the same-ish(accents n shit) as the character under cursor? Correct: else error. 
+
+Feed in any text. 
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::io::prelude::*;
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let word = String::from("Hello");
+        let size = word.len();
+        let letters = word.chars();
+        let duplex = rx.into_iter().zip(letters);
+        let mut errors = 0;
+        let mut counter = 0;
+        for (a, b) in duplex.peekable() {
+            println!("Received:{}, Actual:{}", a, b);
+            if a != b { errors += 1}
+            counter += 1;
+            if counter == size {
+                break;
+            }
+        }
+        println!("Error count: {}", errors);
+        std::process::exit(0)
+    });
+
+    for line in io::stdin().lock().lines() {
+        for letter in line.unwrap().chars() {
+            tx.send(letter);
+        }
+    }
+}
